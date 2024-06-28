50f6b74cdd56d5ee8bb12d0d6983ba30f884afb2
dhanu1402 <dhanukumar2000@gmail.com>
2024-06-28 17:14:28 +0530

    history saved to local storage

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,11 +1,21 @@
-import { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [videos, setVideos] = useState([]);
 
-  const [searchHistory, setSearchHistory] = useState([]);
+  const [searchHistory, setSearchHistory] = useState(() => {
+    // Load search history from localStorage if it exists
+    const savedHistory = localStorage.getItem('searchHistory');
+
+    return savedHistory ? JSON.parse(savedHistory) : [];
+  });
+
+  useEffect(() => {
+    // Save search history to localStorage whenever it changes
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+  }, [searchHistory]);
 
   const addSearchHistory = (handle) => {
     setSearchHistory([...searchHistory, handle]);
